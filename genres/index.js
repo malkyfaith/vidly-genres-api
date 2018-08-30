5578ba7e55d63ac8a702607185da208984d4a345
malkes <malkyhaina@gmail.com>
2018-08-30 21:49:05 +1000

    refactored

--- genres/index.js
+++ genres/index.js
@@ -1,18 +1,56 @@
 const express = require('express');
-const genreRoutes = express.Router();
+const genresRoute = express.Router();
+const Joi = require('joi');
 const { Genre } = require('../models/genre');
-// genreRoutes.get('/', (req, res) => {
-//     const genres = await Genre.find();
-//     res.send(genres);
-// });
 
-// genreRoutes.post('/', (req, res) => {
-//     console.log(req.body);
-//     const genre = new Genre(req.body);
-//     const genres = await save.save();
-//     res.send(genres);
-// });
+const genres = [{ id: 1, name: 'Action' }, { id: 2, name: 'Horror' }, { id: 3, name: 'Comdey' }];
+genresRoute.get('/', (req, res) => {
+    res.send(genres);
+})
+
+genresRoute.post('/', (req, res) => {
+    const { error } = validateGenres(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+})
+
+
+genresRoute.get('/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Genre not found');
+    res.send(genre);
+});
+
+genresRoute.delete('/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Genre not found');
+    genres.splice(genre, 1);
+    res.send(genre);
+});
+
+genresRoute.put('/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Genre not found');
+
+    const { error } = validateGenres(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+function validateGenres(genre) {
+    return Joi.validate(genre, {
+        name: Joi.string().min(3).required()
+    })
+}
 
 
 
-module.exports = { genreRoutes };
+module.exports = { genresRoute };
--- index.js
+++ index.js
@@ -1,71 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Joi = require('joi');
-const { genreRoutes } = require('./genres/')
+
+const { genresRoute } = require('./genres/')
 // create app
 const app = express();
 
 // middleware
 app.use(express.json());
 
-const genres = [{
-    id: 1,
-    name: 'Action'
-}, {
-    id: 2,
-    name: 'Horror'
-}, {
-    id: 3,
-    name: 'Comdey'
-}
-]
+app.use('/api/genres', genresRoute);
 
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-})
 
-app.post('/api/genres', (req, res) => {
-    const { error } = validateGenres(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
-    res.send(genre);
-})
-//app.use('/api/genres', genreRoutes);
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('Genre not found');
-    res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('Genre not found');
-    genres.splice(genre, 1);
-    res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('Genre not found');
-
-    const { error } = validateGenres(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    genre.name = req.body.name;
-    res.send(genre);
-});
-
-function validateGenres(genre) {
-    return Joi.validate(genre, {
-        name: Joi.string().min(3).required()
-    })
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
